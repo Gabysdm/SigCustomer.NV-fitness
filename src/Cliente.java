2e4e4c776a5b2ab8d45921bbc9bf918540aacad8
Gabysdm <gabrielly.lindalva@escolar.ifrn.edu.br>
2025-02-19 08:26:26 -0300

    Mudanças no código

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,31 +1,73 @@
-public class Cliente extends Usuario{
-    String pacoteSelecionado;
 
-    public Cliente(String pacoteSelecionado, String nome, String cpf, String email, String senha) {
-        super(nome, cpf, email, senha);
-        this.pacoteSelecionado = pacoteSelecionado;
+import java.util.Calendar;
+
+public class Cliente {
+    
+    String nome;
+    String celular;
+    String cpf;
+    String email;
+    String senha;
+    Calendar dataDeInscrição;
+    boolean atividade;
+
+    public Cliente(String nome, String celular, String cpf, String email, String senha) {
+        this.nome = nome;
+        this.celular = celular;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
     }
 
-
-    public String getPacoteSelecionado() {
-        return pacoteSelecionado;
+    public String getNome() {
+        return nome;
     }
 
-    public void setPacoteSelecionado(String pacoteSelecionado) {
-        this.pacoteSelecionado = pacoteSelecionado;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
     }
     
-    @Override
-    public void autenticar(){
-        System.out.println("Cliente autenticado.");
-    }
-    @Override
-    public void assinarPacote(){
-        System.out.println("Pacote assinado.");
+    
+    
+    public String getCpf() {
+        return cpf;
     }
 
-    void consultarpacote() {
-        System.out.println("Consultando pacote: " + pacoteSelecionado);
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
     
+    public void cadastrar(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome=" + nome + ", celular=" + celular + ", cpf=" + cpf + ", email=" + email + ", senha=" + senha + ", dataDeInscri\u00e7\u00e3o=" + dataDeInscrição + ", atividade=" + atividade + '}';
+    }
+
 }
--- src/DescontoFuncionario.java
+++
@@ -1,5 +0,0 @@
-public interface DescontoFuncionario {
-    
-    public void aplicarDesconto();
-    
-}
